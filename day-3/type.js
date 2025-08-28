adb758d7c1456197ff5adf986346e78b9adb6fb4
Hope Clarke <thehopeclarke@gmail.com>
2025-08-27 20:53:30 -0500

    completed problems

--- day-3/type.js
+++ day-3/type.js
@@ -14,9 +14,16 @@
  *
  * HINT: There is a method that can help with this.
  */
+
+//i-an input value, may be any type of data
+//o- boolean dependent on if the input is an Array or not
+//c- 
+//e-
+
 function isArray(value) {
   // YOUR CODE BELOW HERE //
-
+  //use Array.isArray() to determine if value is an array
+ return Array.isArray(value);
   // YOUR CODE ABOVE HERE //
 }
 
@@ -34,9 +41,22 @@ function isArray(value) {
  * isObject([1, 2, 3]); // false
  * 
  */
+//i- an input value, may be any type of data
+//o- a boolean dependent on if the input is true object or not
+//c-instance of is a good hint
+//e-
 function isObject(value) {
   // YOUR CODE BELOW HERE //
-  
+  //will check if object is an array and return false
+  if(Array.isArray(value)) return false;
+  //will check if an object is null, return false
+  if(value === null) return false;
+  //will check if object is Date, return false
+  if (value instanceof Date) return false;
+  //remaining value can check object other types of data
+  if(typeof value === 'object') return true;
+  //if nothing else, return false
+  return false;
   // YOUR CODE ABOVE HERE //
 }
 
@@ -46,9 +66,24 @@ function isObject(value) {
  *
  * TIP: Similar to isObject, but we must return true if the value is an Array.
  */
+
+//i- an input value, can be any kind of data
+//o- true if an array or collection object, otherwise false
+//c-
+//e-
 function isCollection(value) {
   // YOUR CODE BELOW HERE //
 
+//will check if an object is null, return false
+if(value === null) return false;
+//will check if object is Date, return false
+if (value instanceof Date) return false;
+//remaining value can check object other types of data
+if(typeof value === 'object') return true;
+//will check if object is an array and return false
+if(Array.isArray(value)) return true;
+//if nothing else, return false
+return false;
   // YOUR CODE ABOVE HERE //
 }
 
@@ -71,9 +106,29 @@ function isCollection(value) {
  *    typeOf("javascript") -> "string"
  *    typeOf([1,2,3]) -> "array"
  */
+
+//i- a value, can be any type of data
+//o- a string that tells you what kind of data it is
+//c-
+//e-
 function typeOf(value) {
   // YOUR CODE BELOW HERE //
-  
+  //check if item is string, return string
+  if(typeof value === 'string') return 'string';
+  //check if item is undefined, return string
+  if(typeof value === undefined) return 'undefined';
+  //check if item is null, return string
+  if(value === null) return 'null';
+  //check if item is function, return string
+  if(typeof value === 'function') return 'function';
+  //check if item is number, return string
+  if(typeof value === 'number') return 'number';
+  //check if item is date, return string
+  if (value instanceof Date) return 'date';
+  //will check if object is an array and return string
+  if(Array.isArray(value)) return 'array';
+  //check if item is an object, return string
+  if(typeof value === 'object') return 'object';
   // YOUR CODE ABOVE HERE //
 }
 
