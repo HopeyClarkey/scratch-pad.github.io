7f6657932a06abbf2413d0c0cfab6e47abd4df06
Hope Clarke <thehopeclarke@gmail.com>
2025-08-28 09:00:40 -0500

    finished problem

--- day-3/homework/range.js
+++ day-3/homework/range.js
@@ -21,9 +21,31 @@
  *  b. you'll need a loop, which one is best?
  *  c. you'll wanna make use of the push() method of Array.
  */
+//i- two integers
+//o- an array containing all the values between the two integers, in reverse if the start is greater
+//c-want to use a loop and push
+//e-
+
 function range(start, end) {
   // YOUR CODE BELOW HERE //
-
+  //create storage array
+  var stArr =[];
+  //if statement to check if end is less than start, with reverse loop
+  if (end < start){
+  //descending for loop if true
+  for (var i = start; i >= end; i--){
+  //push values onto storage array
+    stArr.push(i);
+    }
+  } else {
+  //ascending for loop if false, 
+  for (var i = start; i <= end; i++){
+  //push the values into the storage array
+  stArr.push(i);
+  }
+}
+  //return array
+return stArr;
   // YOUR CODE ABOVE HERE //
 }
 
