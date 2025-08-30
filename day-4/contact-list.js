d7b3302f530983660b28fe1d50df1d9d20396eb3
Hope Clarke <thehopeclarke@gmail.com>
2025-08-29 22:47:19 -0500

    bulldozing find contact

--- day-4/contact-list.js
+++ day-4/contact-list.js
@@ -14,13 +14,17 @@
  *     that manages contacts. The contact-list object should have the following methods:
  *
  *      1. length(): returns the number of contacts within the list. (the list is `contacts`)
+ * 
  *      2. addContact(contact): takes a contact object to be added to the
  *         contact-list.
+ * 
  *      3. findContact(fullName): takes a full-name String, like 'Max Gaudin', and
  *         returns the contact object if found in the contacts-list, or,
  *         undefined if the fullName does not match any contacts in the list.
+ * 
  *      4. removeContact(contact): takes a contact object to be removed from
  *         the contact-list.
+ * 
  *      5. add a printAllContactNames() Function to your makeContactList() factory. The printAllContactNames() Function should
  *         return a String formated with all the full-names of the separated
  *         with a line-break, like so:
@@ -54,7 +58,7 @@ function makeContact(id, nameFirst, nameLast) {
 
 // i-
 //o- object that manages contacts
-//c- must use methods: length- to find the number of contacts within the list
+//c- must use methods: length- to find the number of contacts within the list 
 //c ct- add contact- takes an object called 'contact' to be added to the contact list
 //c ct- findcontact-takes a full name string called 'fullname'- this should return the contact object if a match is found within the object, and undefined if it does not
 //c ct- removeContact- this would remove the object 'contact'
@@ -66,29 +70,42 @@ function makeContactList() {
    * You need something here to hold contacts. See length api for a hint:
    */
   var contacts = [];
-  //length requirement
+  // length requirement
   return {
     // we implemented the length api for you //
     length: function(){
       return contacts.length;
     },
-  //add contact requirement
+
+  // add contact requirement
     addContact: function (contact){
       //takes the contacts array, and finds the end by calling length, and updates to the contact provided????
       contacts[contacts.length] = contact;
     },
-  //find contact requirement
-    findContact: function (fullName){
 
-    },
-  //remove contact requirement
+  // find contact requirement
+  // create function itself
+    findContact: function (fullName){
+  // split full name into search components
+      var searchName = fullName.split(' ');
+  // loop through contact list 
+      for (var i = 0; i < contacts.length; i++){
+  // create variable that represents i
+        var contact = contacts[i];
+  // if statement to check if split name matches first and last name, using new contact variable to 'point' to [i]
+      if (contact[nameFirst] === searchName[0] && contact[nameLast] === searchName[1]);
+      };
+    };
+
+    };
+  // remove contact requirement
     removeContact: function (contact){
 
-    },
+    };
   //print requirement- does not need a parameter because it's just doing something??id*k
     printAllContactNames: function (){
 
-    }
+    };
   }
 
 }
